9765d12e08d0d05f21002a24ffaa4551ec2ed596
Iqbal Fasri <iqbalx64@gmail.com>
2019-01-23 16:17:44 +0700

    Change type for relations service

--- models/Service.model.js
+++ models/Service.model.js
@@ -1,23 +1,27 @@
 const mongoose = require("mongoose");
 
-const ServiceSchema = mongoose.Schema({
-  tanggal_service: {
-    type: Date,
-    required: true
+const ServiceSchema = mongoose.Schema(
+  {
+    tanggal_service: {
+      type: Date,
+      required: true
+    },
+    // Relations
+    no_polisi: {
+      type: mongoose.Types.ObjectId,
+      ref: "Kendaraan",
+      required: true
+    },
+    montir: {
+      // type: mongoose.Types.ObjectId,
+      type: String,
+      ref: "Montir",
+      required: true
+    }
   },
-  // Relations
-  no_polisi:{
-    type: mongoose.Types.ObjectId,
-    ref: 'Kendaraan',
-    required: true
-  },
-  montir: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Montir',
-    required: true
+  {
+    versionKey: false
   }
-}, {
-  versionKey: false
-});
+);
 
 module.exports = mongoose.model("Service", ServiceSchema);
