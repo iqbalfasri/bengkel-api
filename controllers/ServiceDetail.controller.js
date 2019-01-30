973cba8ba2b2fa641100e27f435b7d09c591fe0d
Iqbal Fasri <iqbalx64@gmail.com>
2019-01-30 08:36:55 +0700

    Fetch all and detail ServiceDetail controller

--- controllers/ServiceDetail.controller.js
+++ controllers/ServiceDetail.controller.js
@@ -1,3 +1,64 @@
-const service_detail = require("../models/ServiceDetail.model");
+const servdetail = require("../models/ServiceDetail.model");
 const { code_response } = require("../utils");
 
+// Index Route
+exports.index = (req, res) => {
+  res.send(200, {
+    list_endpoints: ["/all", "/detail/:_id"]
+  });
+};
+
+// All Service Detail
+exports.all = (req, res) => {
+  servdetail
+    .find()
+    .then(servd => {
+      res.send(200, {
+        data: servd,
+        message: "Success",
+        code: code_response.CODE_SUCCESS
+      });
+    })
+    .catch(err => {
+      res.send(500, {
+        message: "Internal server error",
+        code: code_response.CODE_SERVER_ERROR
+      });
+    });
+};
+
+// Detail Service
+exports.detail = (req, res) => {
+  const { _id } = req.params;
+
+  servdetail
+    .findOne({ _id: _id })
+    .populate("service barang_jasa")
+    .then(servd => {
+      if (servd === null) {
+        return res.send(404, {
+          message: "Service tidak ditemukan",
+          code: code_response.CODE_NOT_FOUND
+        });
+      }
+
+      res.send(200, {
+        data: servd,
+        message: "Success",
+        code: code_response.CODE_SUCCESS
+      });
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId") {
+        return res.send(404, {
+          message: "Service tidak ditemukan",
+          code: code_response.CODE_NOT_FOUND
+        });
+      }
+
+      res.send(500, {
+        message: "Internal server error",
+        code: code_response.CODE_SERVER_ERROR
+      });
+    });
+};
--- routes/service-detail.js
+++ routes/service-detail.js
@@ -4,23 +4,23 @@ const router = new (require("restify-router")).Router();
 const {
   index,
   all,
-  create,
-  detail,
-  update,
-  remove
+  // create,
+  // detail,
+  // update,
+  // remove
 } = require("../controllers/ServiceDetail.controller");
 
 // Index Route
-router.get("/", index);
+router.post("/", index);
 // Detail Service Detail
 router.post("/detail/:_id", detail);
 // View / List All Service Detail
 router.post("/all", all);
 // Create BarangJasa / Add new Service Detail
-router.post("/create", create);
+// router.post("/create", create);
 // Update Service Detail
-router.put("/update/:_id", update);
+// router.put("/update/:_id", update);
 // Delete / Remove Service Detail
-router.del("/delete/:_id", remove);
+// router.del("/delete/:_id", remove);
 
 module.exports = router;
