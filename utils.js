4200771074ac1cd2f9d8e422328b82c5bbd16202
Iqbal Fasri <iqbalx64@gmail.com>
2019-02-11 10:11:26 +0700

    all schema validation done

--- utils.js
+++ utils.js
@@ -5,6 +5,32 @@ exports.schemaValidation = {
     nama_barang_jasa: Joi.string().required(),
     harga: Joi.number().required(),
     stok: Joi.number().required()
+  }),
+  customer: Joi.object().keys({
+    nama_customer: Joi.string().required(),
+    alamat_customer: Joi.string().required(),
+    jenis_kelamin_customer: Joi.string().valid("P", "L")
+  }),
+  kendaraan: Joi.object().keys({
+    no_polisi: Joi.string().required(),
+    merk: Joi.string().required(),
+    warna: Joi.string().required(),
+    customer: Joi.string().required()
+  }),
+  montir: Joi.object().keys({
+    nama_montir: Joi.string().required(),
+    alamat_montir: Joi.string().required(),
+    jenis_kelamin_montir: Joi.string().valid("P", "L")
+  }),
+  service: Joi.object().keys({
+    tanggal_service: Joi.date().required(),
+    kendaraan: Joi.string().required(),
+    montir: Joi.string().required()
+  }),
+  serviceDetail: Joi.object().keys({
+    jumlah: Joi.number().required(),
+    service: Joi.string().required(),
+    barang_jasa: Joi.string().required()
   })
 };
 
